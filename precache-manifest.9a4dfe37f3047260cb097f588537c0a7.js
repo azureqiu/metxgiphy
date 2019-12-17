68623433331470ee69ecd7459ad3aceea351d027
Qiuji Zhao <qiujizhao@Qiujis-MacBook-Pro.local>
2019-12-17 10:50:48 -0500

    build

---
+++ precache-manifest.9a4dfe37f3047260cb097f588537c0a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74653e61a1f1e311a7be6b8f299c170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e94a7c9a794609cd9a",
+    "url": "/static/css/main.e6184dde.chunk.css"
+  },
+  {
+    "revision": "1e5328d2dd496dfc7949",
+    "url": "/static/js/2.168f3519.chunk.js"
+  },
+  {
+    "revision": "66e94a7c9a794609cd9a",
+    "url": "/static/js/main.a8b85941.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
